a6fa4001b394449017ec00f9982de61561cf736e
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-22 12:23:51 -0500

    refactored TopBar

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,90 @@
 import React from 'react';
+import styled from 'styled-components';
 
-// Refactor this component to use styled components and not classNames. 
-// You can find the corresponding CSS in the CSS/index.css file
+const TopBarDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`;
+
+const ContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+const ContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+const ContainerSpan = styled.div`
+  cursor: pointer;
+  font-weight: ${props => props.left ? '700' : '100'}
+  margin-right: ${props => props.left ? '25%' : props.center ? '5%' : '0' }
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TopBarDiv>
+      <Container>
+        <ContainerLeft>
+          <ContainerSpan left>TOPICS</ContainerSpan>
+          <ContainerSpan left>SEARCH</ContainerSpan>
+        </ContainerLeft>
+        <ContainerCenter>
+          <ContainerSpan center>GENERAL</ContainerSpan>
+          <ContainerSpan center>BROWNBAG</ContainerSpan>
+          <ContainerSpan center>RANDOM</ContainerSpan>
+          <ContainerSpan center>MUSIC</ContainerSpan>
+          <ContainerSpan center>ANNOUNCEMENTS</ContainerSpan>
+        </ContainerCenter>
+        <ContainerRight>
+          <ContainerSpan right>LOG IN</ContainerSpan>
+        </ContainerRight>
+      </Container>
+    </TopBarDiv>
   )
 }
 
